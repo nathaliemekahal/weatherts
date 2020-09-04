c6cb0f0b94ca373e33bae4b47968f4017466e4b0
Nathalie <nathaliemekahal@gmail.com>
2020-09-04 14:27:30 +0200

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
+
 import "../Login.css";
 
-export class Login extends Component {
-  userHandler = () => {};
+interface loginState {
+  info: info;
+}
+interface info {
+  email: String;
+  password: String;
+}
+export class Login extends Component<any, loginState> {
+  state = {
+    info: {
+      email: "",
+      password: "",
+    },
+  };
+
+  userHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.target.id === "email"
+      ? this.setState({ info: { ...this.state.info, email: e.target.value } })
+      : this.setState({
+          info: { ...this.state.info, password: e.target.value },
+        });
+  };
+  loginUser = async () => {
+    const response = await fetch("http://localhost:3001/users/login", {
+      method: "POST",
+      headers: new Headers({
+        "Content-type": "application/json",
+      }),
+      body: JSON.stringify(this.state.info),
+    });
+    if (response.ok) {
+      this.props.history.push("/home");
+    }
+  };
   render() {
     return (
       <Container id="login">
@@ -19,7 +52,7 @@ export class Login extends Component {
           <p>Password</p>
           <input onChange={this.userHandler} id="password" type="password" />
         </div>
-        <button>Login</button>
+        <button onClick={this.loginUser}>Login</button>
         <p id="registerRoute">
           Not a Member yet ? <a href="/register">Register now</a>
         </p>
